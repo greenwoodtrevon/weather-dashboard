68072ad6852c42f2d51a1f761530a0d688a737be
Trev <greenwoodtrevon@gmail.com>
2021-06-27 20:42:51 -0600

    added styling for all elements that will be added dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,36 @@
-var exampleFunction = function() {
+// var exampleFunction = function() {
+//   fetch(
+//     'https://api.openweathermap.org/data/2.5/weather?q=Draper&appid=27c6c91d331fffec8a4a1d50369b4812'
+//   )
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(response) {
+//       var lat = response.coord.lat;
+//       var lon = response.coord.lon;
+//       console.log(lat);
+//       console.log(lon);
+
+//       fetch(
+//         'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly&appid=27c6c91d331fffec8a4a1d50369b4812'
+//       )
+//         .then(function(response) {
+//           return response.json();
+//         })
+//         .then(function(repsonse) {
+//           console.log(response);
+//         })
+//     });
+//   };    
+ //onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}');
+
+var searchBar = document.getElementById("search-bar");
+var searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", function() {
+  var srchTerm = searchBar.value;
   fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=Draper&appid=27c6c91d331fffec8a4a1d50369b4812'
+    'https://api.openweathermap.org/data/2.5/weather?q=' + srchTerm + '&appid=27c6c91d331fffec8a4a1d50369b4812'
   )
     .then(function(response) {
       return response.json();
@@ -11,24 +41,15 @@ var exampleFunction = function() {
       console.log(lat);
       console.log(lon);
 
-      fetch(
-        'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly&appid=27c6c91d331fffec8a4a1d50369b4812'
-      )
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(repsonse) {
-          console.log(response);
-        })
-    });
-  };    
- //onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}');
-
-var searchBar = document.getElementById("search-bar");
-var searchBtn = document.getElementById("searchBtn");
-
-searchBtn.addEventListener("click", function() {
-  var srchTerm = searchBar.value;
-  exampleFunction();
+  fetch(
+    'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly&appid=27c6c91d331fffec8a4a1d50369b4812'
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(repsonse) {
+      console.log(response);
+    })
+  });
   console.log(srchTerm);
-})
+});
